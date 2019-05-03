fe843559265774888ea41326fc77dd97cf6e5b7b
oonray <alexanderbjornsrud@gmail.com>
2019-05-03 10:26:10 +0200

    fixed colorama

--- htbmanage.py
+++ htbmanage.py
@@ -233,7 +233,7 @@ if(base_arg.local):
             raise ValueError("The machine does not exist!")
 
     if(args.kill_vpn):
-        os.popen('for i in $(ps -aux | grep openvpn'+"{}/vpn.ovpn | awk".format(CONFIG_PATH)+'\'{print $2}\'); do kill $i; done')
+        os.popen('for i in $(ps -aux | grep openvpn'+"{}/vpn.ovpn | awk ".format(CONFIG_PATH)+'\'{print $2}\'); do kill $i; done')
 
     if(args.add_host):
         if(not args.ip):
