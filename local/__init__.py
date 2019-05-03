b8d05857f4d3408232c96643ce6f1c90589ac4ba
oonray <alexanderbjornsrud@gmail.com>
2019-05-03 14:29:05 +0200

    Fixed typo

--- local/__init__.py
+++ local/__init__.py
@@ -112,9 +112,9 @@ class localget(onlineget):
                 os.popen("openvpn {}".format(os.path.join(CONFIG_PATH,"vpn.ovpn")))
             ps = os.popen("ps -aux | grep openvpn {}/vpn.ovpn | awk '{print $2}'".format(CONFIG_PATH)).read()
             print(ps)
-            getter.conf["vpnid"] = ps
-            getter.conf["last"] = name
-            getter.write(CONFIG_PATH+CONFIG_FILE)  
+            self.conf["vpnid"] = ps
+            self.conf["last"] = name
+            self.write(CONFIG_PATH+CONFIG_FILE)  
         except Exception as e:
             print(e)
             exit()
