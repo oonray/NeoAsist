179547f9fb573a80217ea52cc6a35c855a876693
oonray <alexanderbjornsrud@gmail.com>
2019-05-03 13:28:38 +0200

    Fixed typo

--- local/__init__.py
+++ local/__init__.py
@@ -62,7 +62,7 @@ class localget(onlineget):
     def get_machine(self,name):
         self.get_active()
         self.get_retired()
-        return json.loads(self.machine+i+".json")
+        return json.loads(self.machine_path[i]+i+".json")
 
     def get_active(self):
         self.Active = os.listdir(os.path.join(MACHINE_PATH,"Active"))
