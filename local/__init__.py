0ad9eb3c69de7a76a57d26bfa71a4acadaef54cf
oonray <alexanderbjornsrud@gmail.com>
2019-05-03 14:30:20 +0200

    Fixed typo

--- local/__init__.py
+++ local/__init__.py
@@ -108,7 +108,7 @@ class localget(onlineget):
         os.chdir(os.path.join(os.path.join(MACHINE_PATH,status),name))
         try:
             os.system("tmux")
-            if(not sum([int(i) for i in getter.conf["vpnid"].split("\n")])>0):
+            if(not sum([int(i) for i in self.conf["vpnid"].split("\n")])>0):
                 os.popen("openvpn {}".format(os.path.join(CONFIG_PATH,"vpn.ovpn")))
             ps = os.popen("ps -aux | grep openvpn {}/vpn.ovpn | awk '{print $2}'".format(CONFIG_PATH)).read()
             print(ps)
