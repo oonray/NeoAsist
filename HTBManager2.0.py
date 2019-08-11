d0fed9bb6c5ec07dec9e82b89f1bd4bf9c83d84b
Your Name <you@example.com>
2019-08-11 17:31:34 +0200

    Added todo list

---
+++ HTBManager2.0.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+:author: Alexander Bjørnsrud <alexanderbjornsrud@gmail.com>
+:file: htbmanage.py
+:date: 11.08.2019
+
+This progam is made to manage the different aspects of Hack The Box Automatically.
+It is designed to make a structured setup for your machines.
+"""
+import argparse
+
+argparser = argparse.ArgumentParser(description="Manages Hack The Box Machines and Hacking Session")
+
+"""
+Start Session
+Start VPN
+Start Last
+Start STD Scans
+"""
+
+"""
+List Machines
+List Active
+List Retired
+List OSCP
+"""
+
+"""
+Download ALL
+Download Active
+Download Retired
+Download OSCP
+"""
+
+"""
+Stop VPN
+"""
+
+"""
+Add Host to /etc/hosts
+"""
+
+"""
+Remove Host form /etc/hosts
+"""
+
+
+
