b4378b87bfac418e4a553d5407c470f0ae3b51ba
Your Name <you@example.com>
2019-08-11 20:15:27 +0200

    Started on functions

--- HTBManager2.0.py
+++ HTBManager2.0.py
@@ -7,28 +7,49 @@
 This progam is made to manage the different aspects of Hack The Box Automatically.
 It is designed to make a structured setup for your machines.
 """
-import variables
-import argparse
+import variables, argparse, json. sys, os
 
 argparser = argparse.ArgumentParser(description="Manages Hack The Box Machines and Hacking Session")
 
 
-url="https://www.hackthebox.eu/"
-machine_url="/machines/get/all"
+base_url="https://www.hackthebox.eu/"
 
+"""
++######
+| MisC
++######
+"""
 def add_token(url,token):
-    return "{}?api_token={}".format(url,token)
+    return "{}/{}?api_token={}".format(base_url,url,token)
 
-print("{}\n{}".format(variables.MACHINE_FOLDER,variables.CONF_FOLDER))
+def get_api_token():
+    with open(os.path.join(variables.CONF_FOLDER,"htb.conf")) as f:
+        key = json.loads(f.read())["key"]
+        if(key=="<your api key here>"):
+            print("You must add a api key to {}/htb.conf".format(variables.CONF_FOLDER))
+            exit()
+        return key
+
+argparser.add_argument("action",help="The action you want to take eg. START, LIST, DOWNLOAD")
+
+"""
++#######
+| Start
++#######
+"""
+start_group = argparser.add_argument_group("START")
 
 """
 Start Session
 """
+start_group.add_argument("-s",help="Session, Machine To start session with")
 def start_session():
     pass
+
 """
 Start VPN
 """
+start_group.add_argument("-v",help="Start VPN conneciton",action="store_true")
 def start_vpn():
    pass
 
@@ -36,6 +57,7 @@ def start_vpn():
 """
 Start Last
 """
+start_group.add_argument("-l",help="Continue the last session", action="store_true")
 def start_last():
     pass
 
@@ -44,10 +66,26 @@ Start STD Scans
 Start Lease
 """
 
-
+"""
++######
+| LIST
++######
+"""
+list_group = argparser.add_argument_group("LIST")
 
 """
 List Machines
+"""
+list_group.add_argument("-m",help="Lists active machines")
+def list_all_machines():
+   url = "/machines/get/all"
+   add_token(url,get_api_token())
+   pass
+
+def print_all_machines(machines):
+    print(machines)
+
+"""
 List Active
 List Retired
 List OSCP
@@ -55,6 +93,12 @@ List OSCP
 
 """
 Download ALL
+"""
+def download_all():
+    pass
+
+
+"""
 Download Active
 Download Retired
 Download OSCP
