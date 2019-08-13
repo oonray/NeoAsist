78a63beef20d438ba3ed9de13e11ba07ed9f3038
Your Name <you@example.com>
2019-08-13 19:07:34 +0200

    .

--- HTBManager2.0.py
+++ HTBManager2.0.py
@@ -127,7 +127,7 @@ Download ALL
 
 def make_directory(machine):
     location = "Active" if machine["retired_date"] == None else "Retired"
-    if machine["name"] in variabes.OSCP.keys():
+    if machine["name"] in variables.OSCP.keys():
         location = "OSCP"
 
     path = os.path.join(variables.MACHINE_FOLDER,
