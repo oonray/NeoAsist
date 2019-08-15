4379f00625cab379624f788560575d35c7005408
Your Name <you@example.com>
2019-08-15 16:46:16 +0200

    .

--- HTBManager2.0.py
+++ HTBManager2.0.py
@@ -27,7 +27,7 @@ def get_config():
          return json.loads(f.read())
 
 def write_config(config):
-    with open(os.path.join(format(variables.CONF_FOLDER,"htb.conf")),"w"):
+    with open(os.path.join(variables.CONF_FOLDER,"htb.conf"),"w"):
         f.write(json.dumps(conf))
 
 def add_token(url,token):
